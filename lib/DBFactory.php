3e3ea02f2fc27b9cfee1db7f3a6e2e0020cfcede
JhumanJ <julien@nahum.net>
2016-02-03 11:45:47 +0000

    Testing git commit and versions

--- lib/DBFactory.php
+++ lib/DBFactory.php
@@ -11,6 +11,7 @@ class DBFactory
 
     public static function getMysqlConnexionWithPDO(){
         //$db = new PDO('mysql:host=localhost;dbname=unisales','root', 'root');
+
         $db = new PDO('mysql:host=localhost;dbname=unisales','unisalesDB', 'Juju-96-');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
